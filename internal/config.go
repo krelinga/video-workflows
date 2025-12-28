15d72f1c6174c9ca60708646d4559d440bce54ec
Andy Kreling <krelinga@gmail.com>
2025-12-28 02:51:17 +0000

    server config.

---
+++ internal/config.go
@@ -0,0 +1,56 @@
+package internal
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+const (
+	EnvLibraryPath  = "VW_LIBRARY_PATH"
+	EnvTemporalHost = "VW_TEMPORAL_HOST"
+	EnvTemporalPort = "VW_TEMPORAL_PORT"
+)
+
+var (
+	ErrPanicEnvNotSet = errors.New("environment variable not set")
+	ErrPanicEnvNotInt = errors.New("environment variable is not an integer")
+)
+
+type TemporalConfig struct {
+	Host string
+	Port int
+}
+
+type ServerConfig struct {
+	Temporal    *TemporalConfig
+	LibraryPath string
+}
+
+func mustGetenv(key string) string {
+	value, ok := os.LookupEnv(key)
+	if !ok {
+		panic(fmt.Errorf("%w: %s", ErrPanicEnvNotSet, key))
+	}
+	return value
+}
+
+func mustGetenvInt(key string) int {
+	valueStr := mustGetenv(key)
+	var value int
+	_, err := fmt.Sscanf(valueStr, "%d", &value)
+	if err != nil {
+		panic(fmt.Errorf("%w: %s", ErrPanicEnvNotInt, key))
+	}
+	return value
+}
+
+func NewServerConfigFromEnv() *ServerConfig {
+	return &ServerConfig{
+		Temporal: &TemporalConfig{
+			Host: mustGetenv(EnvTemporalHost),
+			Port: mustGetenvInt(EnvTemporalPort),
+		},
+		LibraryPath: mustGetenv(EnvLibraryPath),
+	}
+}
